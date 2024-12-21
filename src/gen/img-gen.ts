b38fffcec3548dda141ca4798fa0137f8f9af4b5
Mikael <mikael.porttila@gmail.com>
2024-12-21 19:32:55 +0100

    skip seed based randomness for better results

--- src/gen/img-gen.ts
+++ src/gen/img-gen.ts
@@ -2,24 +2,22 @@ import { statSync } from "fs";
 import type { FileGeneratorDetails } from "../types/file-gen-details";
 import { getAvgFactorForTargetSize } from "../utilities/sampling";
 import { CircleBuffer } from "../utilities/circle-buffer";
-import { KB } from "../const/file-sizes";
+import { KB, MB } from "../const/file-sizes";
 
 export async function generateImageFile(parameters: FileGeneratorDetails): Promise<void> {
     return new Promise(async (resolve, reject) => {
         const { Jimp, rgbaToInt } = await import('jimp');
-        const seedrandom = await import('seedrandom');
 
         const applyNoise = (image: any): void => {
             const width = image.bitmap.width;
             const height = image.bitmap.height;
-            const rng = seedrandom.default('noise-ly');
 
             for (let y = 0; y < height; y++) {
                 for (let x = 0; x < width; x++) {
-                    const noiseIntensity = rng() * 100;
-                    const r = Math.floor(rng() * noiseIntensity);
-                    const g = Math.floor(rng() * noiseIntensity);
-                    const b = Math.floor(rng() * noiseIntensity);
+                    const noiseIntensity = Math.random() * 100;
+                    const r = Math.floor(Math.random() * noiseIntensity);
+                    const g = Math.floor(Math.random() * noiseIntensity);
+                    const b = Math.floor(Math.random() * noiseIntensity);
 
                     const color = rgbaToInt(r, g, b, 255);
                     image.setPixelColor(color, x, y);
@@ -28,18 +26,25 @@ export async function generateImageFile(parameters: FileGeneratorDetails): Promi
         }
 
         let height = 100;
-        let width = height * 4; 
+        let width = height * 20;
         let crop = false;
         let tries = 0;
-        let sampler = new CircleBuffer<number>(10);
+        let sampler = new CircleBuffer<number>(5);
         let img = new Jimp({ height: height, width: width, color: 0xff0000ff });
         let grow = true;
 
         let maxDelta = 0.0006;
-        if (parameters.sizeInBytes >= KB) {
+        if (parameters.sizeInBytes >= MB) {
             maxDelta = 0.00008;
+        } else if (parameters.sizeInBytes > (10 * KB)) {
+            maxDelta = 0.00008;
+        } else if (parameters.sizeInBytes > KB) {
+            maxDelta = 0.0015;
+        } else if (parameters.sizeInBytes < KB) {
+            maxDelta = 0.0015;
         }
 
+        let overshootCount = 0;
         while (true) {
             try {
                 // Upscale or crop image
@@ -62,7 +67,7 @@ export async function generateImageFile(parameters: FileGeneratorDetails): Promi
                 const diff = sizeDiffInBytes / parameters.sizeInBytes;
                 if (diff <= maxDelta) {
                     if (parameters.debug) {
-                        console.log({ op: `Done (${tries})`, size: `${width}x${width}`, diff });
+                        console.log({ op: `Done (${tries})`, size: `${width}x${height}`, diff, fileSize: `${(fileSizeInBytes / 1024).toFixed(2)} KB` });
                     }
                     resolve();
                     return;
@@ -72,41 +77,63 @@ export async function generateImageFile(parameters: FileGeneratorDetails): Promi
                 crop = fileSizeInBytes > parameters.sizeInBytes;
                 if (crop) {
                     if (grow) {
-                        // Reset sampling
-                        grow = false;
                         sampler.reset();
-                    } else {
-                        sampler.add(fileSizeInBytes);
+                        grow = false;
                     }
 
-                    let pixelsToRemove = 2;
+                    sampler.add(fileSizeInBytes);
+
+                    let pixelsToRemove = 1;
                     if (sampler.isFilled()) {
-                        pixelsToRemove = getAvgFactorForTargetSize(sampler.get(), sizeDiffInBytes);
+                        let factor = 0.8 - ( overshootCount * 0.1);
+
+                        // Lower the risk overshoot when sub MB
+                        if (sizeDiffInBytes < MB) {
+                            factor = 0.4;
+                        }
+
+                        if (factor < 0.2) {
+                            factor = 0.2;
+                        }
+
+                        pixelsToRemove = getAvgFactorForTargetSize(sampler.get(), sizeDiffInBytes, factor);
                         if (parameters.debug) {
-                            console.log({ op: `Shrink (${tries})`, size: `${width}x${width}`, diff });
+                            console.log({ op: `Shrink (${tries})`, size: `${width}x${height}`, diff });
                         }
                     } else {
                         if (parameters.debug) {
-                            console.log({ op: `Shrink (${tries})`, size: `${width}x${width}`, diff }, '\t- Sampling');
+                            console.log({ op: `Shrink (${tries})`, size: `${width}x${height}`, diff }, '(Sampling)');
                         }
                     }
 
                     // Crop from the side which impact the least number of pixels to prevent major size jumps.
                     if (width > height) {
-                        width -= pixelsToRemove;
+                        if (pixelsToRemove < width) {
+                            width -= pixelsToRemove;
+                        }
                     } else {
-                        height -= pixelsToRemove;
+                        if (pixelsToRemove < height) {
+                            height -= pixelsToRemove;
+                        }
                     }
                 } else {
-                    grow = true;
+                    if (!grow) {
+                        grow = true;
+                        overshootCount++;
+                        if (parameters.debug) {
+                            console.log({ op: 'Overshoot', size: `${width}x${height}`, diff, count: overshootCount });
+                        }
+                    }
+
                     const factor = parameters.sizeInBytes / fileSizeInBytes;
                     const dimensionScaleFactor = Math.sqrt(factor);
                     if (parameters.debug) {
-                        console.log({op: `Growth (${tries})`,  size: `${width}x${width}`, diff });
+                        console.log({ op: `Growth (${tries})`, size: `${width}x${height}`, diff });
                     }
 
-                    width = Math.max(width + 1, Math.floor(width * dimensionScaleFactor));
-                    height = Math.max(height + 1, Math.floor(height * dimensionScaleFactor));
+                    // Note: We want to add some extra size to feed the sampler and prevent direct overshoot.
+                    width = Math.max(width + 1, Math.floor(width * (dimensionScaleFactor * 1.05)));
+                    height = Math.max(height + 1, Math.floor(height * dimensionScaleFactor * 1.05));
                 }
 
                 tries++;
